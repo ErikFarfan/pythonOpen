c70ce9553ffbd08a3d00abdc3ed12bbe78b2b58f
Erik Farfan <ingenieroefm@outlook.com>
2022-10-04 21:49:41 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+lista=[]
+
+for numero in range(1,101):
+
+    lista.append(numero)
+    #nlista=sorted(lista,reverse=True)
+    #print(lista2)
+
+lista=sorted(lista,reverse=True)
+print(lista)
+
+for i in lista:
+    print(i)
+
